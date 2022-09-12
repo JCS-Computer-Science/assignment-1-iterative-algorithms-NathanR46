7de59223c2a775c741ad81af40656d05a4b1388a
Nathan Ramsay <104332005@edu.cbe.ab.ca>
2022-09-12 11:58:28 -0600

    Started on Selection Sort Alg

--- selection-sort.js
+++ selection-sort.js
@@ -1,12 +1,16 @@
 function selectionSort(array) {
-	/*
-	 ** Search through the whole array for the smallest element
-	 ** then swap it with the first element.
-	 ** Next, search through the whole array (except the first element)
-	 ** for the smallest, then swap it with the second element.
-	 ** Continue like this until you have only one element left.
-	 */
+	// need to make while loop to loop min swap but also ignore the first index on the second run and the second index on the third run...
+	while (condition) {
+		
+	}
 
+	// code that takes smallest and swaps it with the first element
+	let smallestElement = Math.min(...array);
+	let indexOfSE = array.indexOf(smallestElement);
+	console.log(indexOfSE);
+	let swapElement = array[indexOfSE];
+	array[indexOfSE] = array[0];
+	array[0] = swapElement;
 	return array;
 }
 
