ea91745680bbf7d7c43390d562632a1147edd506
Nathan Ramsay <104332005@edu.cbe.ab.ca>
2022-09-20 09:14:25 -0600

    bubbleSort template made

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,14 +1,8 @@
 function bubbleSort(array) {
-	/*
-    ** Compare the first two elements and swap if necessary.
-    ** Then compare the second and third elements and swap if
-    ** necessary. Continue until you've checked and swapped (if needed)
-    ** the last and second last elements.
-    
-    ** Repeat the above process until you get through an entire
-    ** cycle without needing any swaps
-    */
-
+	// make swapper
+    // make for loop so that it bubble sorts then ignores the last element after sorting
+    // get thing that checks if no swaps were made
+    // smile cuz it works
 	return array;
 }
 
